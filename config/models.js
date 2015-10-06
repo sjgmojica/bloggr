fdb4fb534596856a6ccf07a33a2aad34a78cb918
sjgmojica <mojicasarahjane@gmail.com>
2015-10-06 11:02:31 +0800

    disable autodate created and updated

--- config/models.js
+++ config/models.js
@@ -29,5 +29,7 @@ module.exports.models = {
   ***************************************************************************/
   // migrate: 'alter'
   
-    connection: 'someMongodbServer'
+    connection: 'someMongodbServer',
+    autoCreatedAt: false,
+    autoUpdatedAt: false,
 };
