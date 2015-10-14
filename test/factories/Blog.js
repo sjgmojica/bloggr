3755df9774d2fbf2a0d9a76817b6b51000c9107a
sjgmojica <mojicasarahjane@gmail.com>
2015-10-14 13:42:24 +0800

    add dummy data

--- test/factories/Blog.js
+++ test/factories/Blog.js
@@ -1,22 +1,16 @@
 module.exports = function (factory) {
   factory.define("blog1")
-    .attr("id", 0, {auto_increment:true})
-    .attr("title", "blog test")
-    .attr("body", "blog body")
-    .attr("dt_post", "Mon 2015-10-12 01:47:10 pm")
-    .attr("userId", "1")
+    .attr("id", 1)
+    .attr("blogTitle", "blog test")
+    .attr("blogBody", "blog body");
 
   factory.define("blog2")
-    .attr("id", 0, {auto_increment:true})
-    .attr("title", "blog test2")
-    .attr("body", "blog body2")
-    .attr("dt_post", "Mon 2015-10-12 01:50:10 pm")
-    .attr("userId", "2")
+    .attr("id", 2)
+    //.attr("blogTitle", "blog test2")
+    .attr("blogBody", "blog body2");
 
-  factory.define("blog2")
-    .attr("id", 0, {auto_increment:true})
-    .attr("title", "blog test2")
-    .attr("body", "blog body2")
-    .attr("dt_post", "Mon 2015-10-12 01:50:10 pm")
-    .attr("userId", "3")
+  factory.define("blog3")
+    .attr("id", 1)
+    .attr("blogTitle", "blog update test")
+    .attr("blogBody", "blog update body");
 }
--- test/factories/User.js
+++ test/factories/User.js
@@ -1,23 +1,28 @@
 module.exports = function (factory) {
   factory.define("user1")
-    .attr("id", 0, {auto_increment: true})
+    .attr("id", 1)
     .attr("email", "sa@chikka.com")
     .attr("password", "123456")
     .attr("firstname", "zah")
     .attr("lastname", "mojica")
-    .attr("desc", "photography")
-    .attr("join_dt", "October-12-2015")
-    .attr("join_tm", "01:40:10");
-
+    .attr("desc", "photography");
+    
   factory.define("active_user").parent("user1")
+    .attr("id", 2)
     .attr("email", "sj@chikka.com")
     .attr("firstname", "");
 
   factory.define("active_user1").parent("user1")
+    .attr("id", 3)
     .attr("email", "s@chikka.com")
     .attr("lastname", "");
 
   factory.define("active_user2").parent("user1")
+    .attr("id", 4)
     .attr("email", "se@chikka.com")
     .attr("desc", "");
+
+  factory.define("active_user3").parent("user1")
+    .attr("firstname", "sarz")
+    .attr("desc", "sample desc");
 }
