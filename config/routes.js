f5ef2ee82a2c313d717399eea80f7bf514f8a416
sjgmojica <mojicasarahjane@gmail.com>
2015-10-14 13:42:01 +0800

    replace single quotes to double quotes

--- config/routes.js
+++ config/routes.js
@@ -33,24 +33,24 @@ module.exports.routes = {
   ***************************************************************************/
   
   /* == index, signup, login ==*/
-  'get /'                   : { view: 'index' },
-  //'get /register'           : 'UserController.signup',
-  //'get /login'              : 'UserController.login',
-  'get /register/success'   : { view: 'user/success'},
-  'post /register'          : 'UserController.create',
-  'post /login'             : 'UserController.processLogin',
-  'get /user/edit/:id'      : 'UserController.edit',
-  'put /user/update/:id'    : 'UserController.update',
-  'get /home'               : 'UserController.index',
-  'get /logout'             : 'UserController.logout',
+  "get /"                   : { view: "index" },
+  //"get /register"           : "UserController.signup",
+  //"get /login"              : "UserController.login",
+  "get /register/success"   : { view: "user/success"},
+  "post /register"          : "UserController.create",
+  "post /login"             : "UserController.processLogin",
+  "get /user/edit/:id"      : "UserController.edit",
+  "put /user/update/:id"    : "UserController.update",
+  "get /home"               : "UserController.index",
+  "get /logout"             : "UserController.logout",
 
  /* == blog ==*/
-  //'get /blog/add'           : 'BlogController.add',
-  'post /blog/create'       : 'BlogController.create',
-  'get /blog/edit/:id'      : 'BlogController.edit',
-  'put /blog/update/:id'    : 'BlogController.update',
-  'delete /blog/delete/:id' : 'BlogController.delete',
-  'get /blog'               : 'BlogController.index',
+  //"get /blog/add"           : "BlogController.add",
+  "post /blog/create"       : "BlogController.create",
+  "get /blog/edit/:id"      : "BlogController.edit",
+  "put /blog/update/:id"    : "BlogController.update",
+  "delete /blog/delete/:id" : "BlogController.delete",
+  "get /blog"               : "BlogController.index",
   
   /***************************************************************************
   *                                                                          *
