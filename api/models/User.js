158493aea59f7a765d075e8d919ba73c825e9ba0
sjgmojica <mojicasarahjane@gmail.com>
2015-10-14 13:50:04 +0800

    remove unncecessary comments

--- api/models/User.js
+++ api/models/User.js
@@ -8,45 +8,7 @@
 module.exports = {
   //schema: true,
   //tableName: 'users',
-  /*
-  attributes: {
-    firstname:{
-      type:'string', 
-      required:true
-    },
-    
-    lastname: {
-      type:'string', 
-      required:true
-    },
-    
-    desc: {
-      type:'string',
-      required: true
-    },
-    
-    join_dt: {
-      type:'string', 
-      required:true
-    },
-    
-    join_tm: {
-      type:'string', 
-      required:true
-    },
-    
-    email: {
-      type:'email', 
-      required:true, 
-      //unique: true
-    },
-    
-    password: {
-      type:'string', 
-      required:true
-    }
-  } */
-
+  
   attributes: {
     email: {
       type: "email",
