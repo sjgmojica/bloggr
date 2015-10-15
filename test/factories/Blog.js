77a5b3402933ca9f3a67ec71b6fb16d6674dd25a
sjgmojica <mojicasarahjane@gmail.com>
2015-10-15 15:10:30 +0800

    add dummy data

--- test/factories/Blog.js
+++ test/factories/Blog.js
@@ -6,11 +6,11 @@ module.exports = function (factory) {
 
   factory.define("blog2")
     .attr("id", 2)
-    //.attr("blogTitle", "blog test2")
-    .attr("blogBody", "blog body2");
+    .attr("blogTitle", "update blog title")
+    .attr("blogBody", "update blog body");
 
   factory.define("blog3")
     .attr("id", 1)
-    .attr("blogTitle", "blog update test")
-    .attr("blogBody", "blog update body");
+    .attr("blogTitle", "")
+    .attr("blogBody", "blog body3");
 }
