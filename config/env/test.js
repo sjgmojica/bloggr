9254e4cf81a5ab1720b61c232e50916c19427953
sjgmojica <mojicasarahjane@gmail.com>
2015-10-20 15:37:26 +0800

    removed unnecessary comment

--- config/env/test.js
+++ config/env/test.js
@@ -1,30 +1,4 @@
 module.exports = {
-/*
- //-- Environment
- environment: "test",
-
- host: "localhost",
-
- //-- Session
- session: {
-   adapter: "memory"
- },
-
- connections: {
-   someMongodbServer: {
-     module: "sails-memory",
-     host: "localhost",
-     user: "",
-     password: "",
-     database: "bloggr_test"
-   }
- },
-
- models: {
-  migrate: "safe"
- }
- */
-
 log: {
   level: "silent"
 },
